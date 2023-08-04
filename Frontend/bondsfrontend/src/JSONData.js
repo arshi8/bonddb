99be76b98930ceea2e0ef842c1d58bcd7eb8c644
hp2309 <hetal.priyadarshi.23@gmail.com>
2023-08-04 17:04:18 +0530

    Update JSONData.js
    Added Trades Test Data along with reformatting of Date and Status of Securities

--- Frontend/bondsfrontend/src/JSONData.js
+++ Frontend/bondsfrontend/src/JSONData.js
@@ -62,44 +62,107 @@ let Security = [
         "Id": 1001,
         "ISIN": "IN123456A78B",
         "CUSIP": 123456121,
-        "Issuer": "ReserveBankofIndia",
-        "MaturityDate": 20230802,
+        "Issuer": "Reserve Bank Of India",
+        "MaturityDate": "2023-08-02",
         "Coupon": "CPNCD001",
         "Type": "GOVT",
         "FaceValue": 100,
-        "Status": 1 //matured 1 else 0
+        "Status": "Matured"
     },
     {
         "Id": 1002,
         "ISIN": "US123456A78C",
         "CUSIP": 123456122,
         "Issuer": "Morgan Stanley",
-        "MaturityDate": 20230812,
+        "MaturityDate": "2023-08-12",
         "Coupon": "CPNCD002",
         "Type": "CORP",
         "FaceValue": 150,
-        "Status": 0 //matured 1 else 0
+        "Status": "Not-Matured"
     },
     {
         "Id": 1003,
         "ISIN": "US123456A78D",
         "CUSIP": 123456123,
-        "Issuer": "BankOfAmerica",
-        "MaturityDate": 20230612,
+        "Issuer": "Bank Of America",
+        "MaturityDate": "2023-06-12",
         "Coupon": "CPNCD003",
         "Type": "GOVT",
         "FaceValue": 75,
-        "Status": 1 //matured 1 else 0
+        "Status": "Matured"
     },
     {
         "Id": 1004,
         "ISIN": "UK123456A78E",
         "CUSIP": 123456124,
         "Issuer": "Barclays",
-        "MaturityDate": 20230724,
+        "MaturityDate": "2023-07-24",
         "Coupon": "CPNCD004",
         "Type": "CORP",
         "FaceValue": 80,
-        "Status": 1 //matured 1 else 0
+        "Status": "Matured"
     }
+]
+
+let Trade = [
+    {
+        "Id":1,
+        "BookId": 1,
+        "CounterpartyId": 1,
+        "SecurityId": 1,
+        "Quantity": 10,
+        "Status": "Settled",
+        "Price": 200,
+        "Buy_Sell": "Buy",
+        "TradeDate": "2021-01-01",
+        "SettlementDate": "2023-06-01" 
+    },
+    {
+        "Id": 2,
+        "BookId": 2,
+        "CounterpartyId": 2,
+        "SecurityId": 2,
+        "Quantity": 35,
+        "Status": "Not-Settled",
+        "Price": 150,
+        "Buy_Sell": "Sell",
+        "TradeDate": "2023-04-20",
+        "SettlementDate": "2023-12-20" 
+    },
+    {
+        "Id": 3,
+        "BookId": 3,
+        "CounterpartyId": 3,
+        "SecurityId": 3,
+        "Quantity": 25,
+        "Status": "Settled",
+        "Price": 100,
+        "Buy_Sell": "Buy",
+        "TradeDate": "2020-10-30",
+        "SettlementDate": "2022-10-30" 
+    },
+    {
+        "Id": 4,
+        "BookId": 4,
+        "CounterpartyId": 4,
+        "SecurityId": 4,
+        "Quantity": 50,
+        "Status": "Settled",
+        "Price": 75,
+        "Buy_Sell": "Sell",
+        "TradeDate": "2022-05-25",
+        "SettlementDate": "2023-03-10" 
+    },
+    {
+        "Id": 5,
+        "BookId": 5,
+        "CounterpartyId": 5,
+        "SecurityId": 5,
+        "Quantity": 60,
+        "Status": "Not-Settled",
+        "Price": 150,
+        "Buy_Sell": "Buy",
+        "TradeDate": "2020-12-15",
+        "SettlementDate": "2024-12-15" 
+    }
 ]
