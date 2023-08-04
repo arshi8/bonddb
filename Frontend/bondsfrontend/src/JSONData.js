0e1af2b67d3a31217bffdbab13dfe785636ad614
hp2309 <hetal.priyadarshi.23@gmail.com>
2023-08-04 16:42:11 +0530

    Update JSONData.js
    Added Test data for CounterParty and Security Items

--- Frontend/bondsfrontend/src/JSONData.js
+++ Frontend/bondsfrontend/src/JSONData.js
@@ -41,4 +41,65 @@ let BookUser = [
         "UserId":7654
     }
 ]
-   
+
+let CounterParty = [
+    {
+        "Id": 1001,
+        "Name": "Google Cloud"
+    },
+    {
+        "Id": 1002,
+        "Name": "Microsoft Azure"
+    },
+    {
+        "Id": 1003,
+        "Name": "Amazon Web Services"
+    }
+]
+
+let Security = [
+    {
+        "Id": 1001,
+        "ISIN": "IN123456A78B",
+        "CUSIP": 123456121,
+        "Issuer": "ReserveBankofIndia",
+        "MaturityDate": 20230802,
+        "Coupon": "CPNCD001",
+        "Type": "GOVT",
+        "FaceValue": 100,
+        "Status": 1 //matured 1 else 0
+    },
+    {
+        "Id": 1002,
+        "ISIN": "US123456A78C",
+        "CUSIP": 123456122,
+        "Issuer": "Morgan Stanley",
+        "MaturityDate": 20230812,
+        "Coupon": "CPNCD002",
+        "Type": "CORP",
+        "FaceValue": 150,
+        "Status": 0 //matured 1 else 0
+    },
+    {
+        "Id": 1003,
+        "ISIN": "US123456A78D",
+        "CUSIP": 123456123,
+        "Issuer": "BankOfAmerica",
+        "MaturityDate": 20230612,
+        "Coupon": "CPNCD003",
+        "Type": "GOVT",
+        "FaceValue": 75,
+        "Status": 1 //matured 1 else 0
+    },
+    {
+        "Id": 1004,
+        "ISIN": "UK123456A78E",
+        "CUSIP": 123456124,
+        "Issuer": "Barclays",
+        "MaturityDate": 20230724,
+        "Coupon": "CPNCD004",
+        "Type": "CORP",
+        "FaceValue": 80,
+        "Status": 1 //matured 1 else 0
+    }
+]
