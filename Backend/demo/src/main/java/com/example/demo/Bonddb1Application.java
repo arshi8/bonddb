3df8dafa6f3a289e7ab5f8f95ae16d9c3a96e9af
Arshi Jindani <arshijindani8@gmail.com>
2023-08-04 16:07:34 +0530

    first commit

---
+++ Backend/demo/src/main/java/com/example/demo/Bonddb1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bonddb1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bonddb1Application.class, args);
+	}
+
+}
