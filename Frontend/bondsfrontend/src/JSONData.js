290fca6eed45648654b83dbda4ec08105f758f74
Jhanvi Desai <jdesai024@gmail.com>
2023-08-04 16:10:42 +0530

    Add dummy JSON Data for Book, BookName and User

---
+++ Frontend/bondsfrontend/src/JSONData.js
@@ -0,0 +1,44 @@
+let Books = [
+    {
+        "Id":1234,
+        "BookName":"XYZ"
+
+    },
+]
+
+let User = [
+    {
+        "Id":9876,
+        "Name":"User1",
+        "Email":"User1@domain.com",
+        "Role": "Admin"
+    },
+    {
+        "Id":8765,
+        "Name":"User2",
+        "Email":"User2@domain.com",
+        "Role": "Admin"
+    },
+    {
+        "Id":7654,
+        "Name":"User3",
+        "Email":"User3@domain.com",
+        "Role": "Admin"
+    }
+]
+
+let BookUser = [
+    {
+        "BookId":1234,
+        "UserId":9876
+    },
+    {
+        "BookId":1234,
+        "UserId":8765
+    },
+    {
+        "BookId":1234,
+        "UserId":7654
+    }
+]
+   
