f82280c7863530b7752941cc29169efcfc32351f
Jhanvi Desai <jdesai024@gmail.com>
2023-08-04 17:10:20 +0530

    Add Dashboard basic structure

--- Frontend/bondsfrontend/src/App.js
+++ Frontend/bondsfrontend/src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Dashboard from './pages/Dashboard';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
+    {/* <Navbar></Navbar> */}
     <Dashboard></Dashboard>
     </>
   );
--- Frontend/bondsfrontend/src/pages/Dashboard.js
+++ Frontend/bondsfrontend/src/pages/Dashboard.js
@@ -1,7 +1,21 @@
+import "./Dashboard.css"
 function Dashboard(){
  return(
     <>
-    <h1>Hello</h1>
+    <div className="main">
+      <div className="bookanduserdetails">
+         <h1>User details</h1>
+      </div>
+      <div className="bookanduserdetails">
+         <h1>Selected books</h1>
+      </div>
+      <div className="tradedetails">
+         <div className="listoftrades">
+      </div>
+      <div className="selectedtradedetails">
+      </div>
+      </div>
+    </div>
     </>
  )
 }
