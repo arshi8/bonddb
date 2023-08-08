f4f05460358f77b01906975dade200644d1f0f4e
Jhanvi Desai <jdesai024@gmail.com>
2023-08-09 00:05:06 +0530

    add date selector error

--- Frontend/bondsfrontend/src/pages/Dashboard.js
+++ Frontend/bondsfrontend/src/pages/Dashboard.js
@@ -40,16 +40,39 @@ console.log(GroupTradesByBook())
 function Dashboard() {
    const [isOpen, setIsOpen] = useState(false);
    const [currentBookId, setCurrentBookID] = useState(0);
+   const [error, setError] = useState('');
 
    const toggleDropdown = () => {
-     setIsOpen(!isOpen);
+      setIsOpen(!isOpen);
    };
 
-   function handledropdown(option){
+   function handledropdown(option) {
       setCurrentBookID(option);
       // console.log(option);
 
    }
+   const [selectedFromDate, setSelectedFromDate] = useState('');
+
+   const handleFromDateChange = (event) => {
+      setSelectedFromDate(event.target.value);
+      setError('');
+   };
+   const [selectedToDate, setSelectedToDate] = useState('');
+
+   const handleToDateChange = (event) => {
+      setSelectedToDate(event.target.value);
+      // setError('');
+   };
+
+   const validateDateRange = () => {
+      if (selectedFromDate && selectedToDate) {
+        if (new Date(selectedToDate) < new Date(selectedFromDate)) {
+          setError('To date must be greater than from date');
+        } else {
+         //  setError('');
+        }
+      }
+    };
 
 
    return (
@@ -66,18 +89,45 @@ function Dashboard() {
                {/* <h1>book selector</h1> */}
                <div className={`dropdown ${isOpen ? 'show' : ''} `}>
                   <button className="btn btn-secondary dropdown-toggle drpdownwdt" type="button" data-toggle="dropdown" onClick={toggleDropdown}>
-                  {/* Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. */}
-                  Lorem Ipsum 
+                     {/* Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. */}
+                     Lorem Ipsum
                   </button>
                   <ul className={`dropdown-menu ${isOpen ? 'show' : ''}`}>
-                  {/* <ul className={`dropdown-menu`}> */}
+                     {/* <ul className={`dropdown-menu`}> */}
 
-                  {BookData.map((item) => (
-                     <li key={item.Id}><div className="dropdown-item" onClick={() => handledropdown(item.Id)}>{item.BookName}</div></li>
-                        ))}
+                     {BookData.map((item) => (
+                        <li key={item.Id}><div className="dropdown-item" onClick={() => handledropdown(item.Id)}>{item.BookName}</div></li>
+                     ))}
                   </ul>
                </div>
-              
+               <div className="dateselectors">
+                  {/* <div className="container mt-5"> */}
+                  {/* <h2>Date Selector using Bootstrap</h2> */}
+
+                  <div className="form-group">
+                     <label>From:</label>
+                     <input
+                        type="date"
+                        className="form-control"
+                        value={selectedFromDate}
+                        onChange={handleFromDateChange}
+                        onBlur={validateDateRange}
+                     />
+                  </div>
+                  <div className="form-group">
+                     <label>To:</label>
+                     <input
+                        type="date"
+                        className="form-control"
+                        value={selectedToDate}
+                        onChange={handleToDateChange}
+                        onBlur={validateDateRange}
+                     />
+                  </div>
+                  {error && <p className="text-danger">{error}</p>}
+                  {/* <p>Selected Date: {selectedDate}</p> */}
+                  {/* </div> */}
+               </div>
             </div>
             <div className="tradedetails">
                <div className="listoftrades">
@@ -106,27 +156,27 @@ function Dashboard() {
                         </tr> */}
                         {TradeData.map((item) => (
                            currentBookId === 0 || currentBookId === item.BookId ? (
-                           <tr key={item.Id}>
-                              <th scope="row">{item.Id}</th>
-                              <td>{item.BookId}</td>
-                              <td>{item.CounterpartyId}</td>
-                              <td>{item.SecurityId}</td>
-                              <td>{item.Quantity}</td>
-                              <td>{item.Status}</td>
-                              <td>{item.Price}</td>
-                              <td>{item.Buy_Sell}</td>
-                              <td>{item.TradeDate}</td>
-                              <td>{item.SettlementDate}</td>
+                              <tr key={item.Id}>
+                                 <th scope="row">{item.Id}</th>
+                                 <td>{item.BookId}</td>
+                                 <td>{item.CounterpartyId}</td>
+                                 <td>{item.SecurityId}</td>
+                                 <td>{item.Quantity}</td>
+                                 <td>{item.Status}</td>
+                                 <td>{item.Price}</td>
+                                 <td>{item.Buy_Sell}</td>
+                                 <td>{item.TradeDate}</td>
+                                 <td>{item.SettlementDate}</td>
 
-                           </tr>
-                           ): null
+                              </tr>
+                           ) : null
                         ))}
                      </tbody>
                   </table>
                </div>
                <div className="selectedtradedetails">
                </div>
-            </div> 
+            </div>
          </div>
       </>
    )
